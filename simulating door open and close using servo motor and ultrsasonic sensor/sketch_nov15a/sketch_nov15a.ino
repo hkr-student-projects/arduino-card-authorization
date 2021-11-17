f45da65e0a31e81e0bf7abb66e4a83e32d1cf1a7
Chidiebere Ibeh <chidiebere.ugonna@gmail.com>
2021-11-17 20:29:43 +0100

    sensor light trigger

--- simulating door open and close using servo motor and ultrsasonic sensor/sketch_nov15a/sketch_nov15a.ino
+++ simulating door open and close using servo motor and ultrsasonic sensor/sketch_nov15a/sketch_nov15a.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 int trigPin=8;
 int echoPin=10;
+int trigLight = 7;
 
 
 
@@ -8,10 +9,10 @@ Servo servo;
 int angle=0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(trigPin,OUTPUT);
   pinMode(echoPin,INPUT);
+  pinMode(trigLight, OUTPUT);
   servo.attach(9);
   servo.write(angle);
   delay(2000);
@@ -31,11 +32,13 @@ void loop() {
   delay(10);
   if(distance<=10){
     servo.write(90);
+    digitalWrite(trigLight,HIGH);
     delay(100);
    
   }
   else if(distance>10){
     servo.write(angle);
+   digitalWrite(trigLight,LOW);
   }
 //servo.write(90);
 //delay(1000);
